8162aca2c301a99b63292918a494881f22c3411d
Rafael Figueiredo <rafael.figueiredo@smarthis.com.br>
2021-02-13 01:19:37 -0300

    initial commit

---
+++ django_cv/cv_app/views.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+
+def login(request):
+    return render(request, 'login.html')
+    
+@login_required()
+def home(request):
+    return render(request, 'home.html')
+
+def logout(request):
+    auth_logout(request)
+    return redirect('/')
+
